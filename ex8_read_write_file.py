eaada28d6b8f687af190a896d0bb4d1d508094ec
Michal Marciniak <gp3510@gmail.com>
2023-08-09 23:42:02 +0200

    ex8 completed

--- ex8_read_write_file.py
+++ ex8_read_write_file.py
@@ -1,15 +1,20 @@
 def writeToFile(file, content):
-    pass
+    with open(file, "w") as fileObj:
+        fileObj.write(content)
 
 
 def appendToFile(file, content):
-    pass
+    with open(file, "a") as fileObj:
+        fileObj.write(content)
 
 
 def readFromFile(file):
-    pass
+    with open(file) as fileObj:
+        return fileObj.read()
 
 
 writeToFile("greet.txt", "Hello!\n")
 appendToFile("greet.txt", "Goodbye!\n")
 assert readFromFile("greet.txt") == "Hello!\nGoodbye!\n"
+
+print(readFromFile("greet.txt"))
